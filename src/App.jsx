208eaa805a960a999d7291b3e73c6e432c9ec4cc
ankitkr52 <ankit72p@gmail.com>
2026-02-13 11:22:32 +0530

    project initilising

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [num, setNum] = useState(12);
+  const [type, setType] = useState("linear")
+  return (
+    <div className='min-h-screen bg-gray-600'>
+      <div className='w-9/12 mx-auto'>
+        <div className='flex justify-between'>
+          <h1 className='text-3xl font-bold'>
+            🎨 Gradient generator
+          </h1>
+          <div >
+            <input value={num} type="text" placeholder='12' onChange={(e) => setNum(Number(e.target.value))} className='border bg-gray-200 rounded-lg w-[200px] p-2 mr-2' />
+            <select value={type} className='border bg-gray-200 rounded-lg w-[100px] p-2' onChange={(e) => setType(e.target.value)}>
+              <option value="linear">linear</option>
+              <option value="radial">Radial</option>
+            </select>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
